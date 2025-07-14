12a712abb97f327cfc79fdc90c59b00f658c1ded
atommnava <atommunoznava@gmail.com>
2025-07-13 22:01:14 -0600

    Cambios

--- ibero/__init__.py
+++ ibero/__init__.py
@@ -1,6 +1,15 @@
 from flask import Flask
 
+
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = ''
+
+
 
 # Deplegar las rutas desde __init__.py
 from ibero import routes
+
