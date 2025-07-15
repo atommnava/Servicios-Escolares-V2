b7ece230254aaff0d5f3a2e568e67e30c45a36ac
atommnava <atommunoznava@gmail.com>
2025-07-14 19:12:38 -0600

    Nuevos cambios

--- ibero/routes.py
+++ ibero/routes.py
@@ -1,7 +1,11 @@
 from flask import render_template
 from ibero import app
 
+@app.route('/login')
+def login_page():
+    return render_template('login.html')
+
 @app.route('/')
 @app.route('/home')
-def home():
+def home_page():
     return render_template('home.html')
